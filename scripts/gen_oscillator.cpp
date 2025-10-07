21d3d5b2e27c41c13db90885e84c011d0a267b88
Rahul Rajesh <rajeshrah22@gmail.com>
2025-10-07 19:28:46 -0400

    re-organized

--- scripts/gen_oscillator.cpp
+++ scripts/gen_oscillator.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <tuple>
 
-auto main() ->  int{
+auto main() ->  int {
     constexpr size_t total_inverters=12;
     std::random_device rd{};
     std::mt19937 rng{rd()};
@@ -11,34 +11,37 @@ auto main() ->  int{
 
     auto in_range = [](auto x, auto mu, auto k)
     { return (mu-k) <= x && x <= (mu+k); };
+
     auto get_random = [&distribution, &rng, in_range](auto range){ double t;
-    do{
-    t = distribution(rng);
-    }while(!in_range(t, 1, range));
-    return t;
+	    do {
+		    t = distribution(rng);
+	    } while(!in_range(t, 1, range));
+
+	    return t;
     };
 
     auto get_size_variations = [get_random]() {
-    return std::make_tuple(get_random(0.15), get_random(0.15), get_random(0.15), get_random(0.15));
+	    return std::make_tuple(get_random(0.15), get_random(0.15), get_random(0.15), get_random(0.15));
     };
 
     auto get_thickess_variations = [get_random]()  {
-    return std::make_tuple(get_random(0.10), get_random(0.10));
+	    return std::make_tuple(get_random(0.10), get_random(0.10));
     };
 
     std::cout <<"* ring oscillator\n.subckt ring_oscillator_5 en n12" << std::endl;
 
     std::cout <<"xNAND en n12 n0 nand2x1" << std::endl;
 
-    for(size_t i=0; i < total_inverters; i++){
-    auto [tplv, tpwv, tnln, tnwn] = get_size_variations();
-    auto [tpotv, tnotv] = get_thickess_variations();
-    std::cout
-    <<"x" << (i+1) << " n" << i << " n" << (i+1)<< " inverter"
-    << " tplv=" << tplv << " tpwv=" << tpwv
-    << " tnln=" << tnln << " tnwn=" << tnwn
-    << " tpotv=" << tpotv << " tnotv=" << tnotv
-    << std::endl;
+    for(size_t i=0; i < total_inverters; i++) {
+	    auto [tplv, tpwv, tnln, tnwn] = get_size_variations();
+	    auto [tpotv, tnotv] = get_thickess_variations();
+	    std::cout
+	    <<"x" << (i+1) << " n" << i << " n" << (i+1)<< " inverter"
+	    << " tplv=" << tplv << " tpwv=" << tpwv
+	    << " tnln=" << tnln << " tnwn=" << tnwn
+	    << " tpotv=" << tpotv << " tnotv=" << tnotv
+	    << std::endl;
     }
+
     std::cout <<".ends\n" << std::endl;
 }
