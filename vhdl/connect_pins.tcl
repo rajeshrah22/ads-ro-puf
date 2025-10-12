485ebaee6ffbd83ab822b5de7437873bba8432f5
Rahul Rajesh <rajeshrah22@gmail.com>
2025-10-12 15:11:09 -0400

    fixed assignment script
    fixed tcl script
    fixed project_pkg errors

--- vhdl/connect_pins.tcl
+++ vhdl/connect_pins.tcl
@@ -4,8 +4,8 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to clock;
 
 # Button
 set_location_assignment PIN_B8 -to reset;
-set_instance_assignment -name IO_STANDARD “3.3 V SCHMITT TRIGGER” -to reset;
+set_instance_assignment -name IO_STANDARD "3.3 V SCHMITT TRIGGER" -to reset;
 
 # LED
-set_location_assignment PIN_PA8 -to done;
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to done;
+set_location_assignment PIN_A8 -to done;
+set_instance_assignment -name IO_STANDARD "3.3-V-LVTTL" -to done;
--- vhdl/project_pkg.vhd
+++ vhdl/project_pkg.vhd
@@ -1,3 +1,6 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 package project_pkg is
 	component ring_oscillator
 		generic (
